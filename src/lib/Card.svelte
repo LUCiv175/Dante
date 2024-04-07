eeed42477d06af35fd35d21322ee2958775e1bcf
LUCiv175 <66305258+LUCiv175@users.noreply.github.com>
2024-04-07 11:03:43 +0200

    List Page Responsive

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,14 +1,11 @@
 <script>
   export let titolo;
   export let autore;
-
+  export let img;
+ 
 </script>
 <style>
-    
-    :root {
-  --card-height: 30rem;
-  --card-width: 20rem;
-}
+   
 * {
   box-sizing: border-box;
 }
@@ -21,28 +18,26 @@ body {
   background: #191c29;
 }
 .card {
-  width: var(--card-width);
-  height: var(--card-height);
+  width: 15rem;
+  height: 22.5rem;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: flex-end;
   padding: 0 36px;
   perspective: 2500px;
-  margin: 0 50px;
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
-  margin-bottom: 5vh;
 }
-
+ 
 .cover-image {
     border-top-left-radius: 15px;
   border-top-right-radius: 15px;
   width: 100%;
-  height: 100%;
-  object-fit: cover;
+  object-fit: cover;  
+  height: 22.5rem;
 }
-
+ 
 .wrapper {
   transition: all 0.5s;
   position: absolute;
@@ -50,14 +45,15 @@ body {
   z-index: -1;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
 }
-
+ 
 .wrapper::before,
 .wrapper::after {
   content: "";
   opacity: 0;
   width: 100%;
-  
+ 
   transition: all 0.5s;
   position: absolute;
   left: 0;
@@ -75,20 +71,21 @@ body {
   background: rgb(255,255,255);
     background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 0%, rgba(0,0,0,0.86) 26%, rgba(0,0,0,0) 100%);
 }
-
+ 
 .card:hover .wrapper::before,
 .wrapper::after {
   opacity: 1;
 }
-
+ 
 .title {
   width: 100%;
   transition: transform 0.5s;
   font-family: "Island Moments", cursive;
-  font-weight: 400;
+  font-weight: 500;
   font-style: normal;
   text-decoration: none;
   color: #FF1B1C;
+  font-size: 1.7rem;
   margin-bottom: 5px;
 }
 .subtitle {
@@ -100,10 +97,10 @@ body {
   font-style: normal;
   text-decoration: none;
   color: #FF9F1C;
-  font-size: 1.75rem;
+  font-size: 1.5rem;
 }
-
-
+ 
+ 
 .card:hover .wrapper {
   transform: perspective(900px) translateY(-5%) rotateX(25deg) translateZ(0);
   box-shadow: 2px 35px 32px -8px rgba(0, 0, 0, 0.75);
@@ -112,7 +109,7 @@ body {
   background: rgb(255,255,255);
   background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 0%, rgba(0,0,0,0.86) 90%, rgba(0,0,0,0) 100%);
 }
-
+ 
 .card:hover .title, .card:hover .subtitle {
   transform: translate3d(0%, -50px, 100px);
   scale: 1.1;
@@ -121,15 +118,103 @@ body {
     display: flex;
         align-items: center;
         justify-content: center;
+        width: 100%;
+}
+ 
+@media screen and (max-width: 1170px){
+    .card{
+        width: 12rem;
+        height: 18rem;
+    }
+    .cover-image {
+        height: 18rem;
+    }
+    .title {
+        font-size: 1.5rem;
+        width: 10rem;
+    }
+    .subtitle {
+        font-size: 1.3rem;
+        width: 10rem;
+    }
+}
+@media screen and (max-width: 950px){
+    .card{
+        width: 10rem;
+        height: 15rem;
+    }
+    .cover-image {
+        height: 15rem;
+    }
+    .title {
+        font-size: 1.3rem;
+        width: 8rem;
+    }
+    .subtitle {
+        font-size: 1.1rem;
+        width: 8rem;
+    }
+ 
+}
+@media screen and (max-width: 690px){
+    .card{
+        width: 8rem;
+        height: 12rem;
+    }
+    .title {
+        font-size: 1.1rem;
+        width: 7rem;
+    }
+    .subtitle {
+        font-size: 0.9rem;
+        width: 7rem;
+    }
+    .cover-image {
+        height: 12rem;
+    }
+}
+@media screen and (max-width: 570px){
+    .card{
+        width: 6rem;
+        height: 9rem;
+    }
+    .title {
+        font-size: 0.9rem;
+        width: 5rem;
+    }
+    .subtitle {
+        font-size: 0.7rem;
+        width: 5rem;
+    }
+    .cover-image {
+        height: 9rem;
+    }
+}
+@media screen and (max-width: 470px){
+    .card{
+        width: 4rem;
+        height: 6rem;
+    }
+    .title {
+        font-size: 0.7rem;
+        width: 3rem;
+    }
+    .subtitle {
+        font-size: 0.5rem;
+        width: 3rem;
+    }
+    .cover-image {
+        height: 6rem;
+    }
 }
 </style>
 <div class="grid-item">
     <div class="card">
       <div class="wrapper">
-        <img src="https://copertine.hoepli.it/archivio/978/8836/9788836005765.jpg" class="cover-image" />
+        <img src={img} class="cover-image" />
       </div>
       <div>
       <h1 class="title">{titolo}</h1>
-      
+     
       <h3 class="subtitle">{autore}</h3></div>
     </div></div>
--- src/lib/list.svelte
+++ src/lib/list.svelte
@@ -1,59 +1,93 @@
 <script>
-    import Card from "./Card.svelte";
+  import Card from "./Card.svelte";
 
 </script>
 
 <style>
-    
-    .hero {
-    position: relative;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    color: #fff;
-    text-align: center;
-    padding: 0 20px;
-    flex-direction: column;
-    background-color: #262626;
-  }
+ 
+  .hero {
+  position: relative;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  text-align: center;
+ 
+  width: 100%;
+  flex-direction: column;
+  background-color: #262626;
+}
 
+.cards{
+  display: grid;
+ 
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      gap: 5px;
+      row-gap: 50px;
+  margin: 0 5px;
+}
+
+
+.hero img {
+  margin: auto;
+  height: 27vh;
+  width:fit-content;
+  margin-bottom: 10px;
+  filter: brightness(1000%);
+}
+
+.hero p {
+  font-size: 2.5rem;
+  font-family: "inter", sans-serif;
+font-weight: 700;
+font-style: normal;
+color: #FF9F1C;
+margin-bottom: 10vh;
+}
+
+@media screen and (max-width: 1500px) {
   .cards{
     display: grid;
-    width: 100%;
-        display: grid;
-        grid-template-columns: repeat(3, 1fr); 
-        gap: 10px;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 4px;
+    row-gap: 20px;
   }
-  
-  
   .hero img {
-    margin: auto;
-    height: 27vh;
-    width:fit-content;
-    margin-bottom: 10px;
-    filter: brightness(1000%);
+    height: 20vh;
   }
-
   .hero p {
-    font-size: 2.5rem;
-    font-family: "inter", sans-serif;
-  font-weight: 700;
-  font-style: normal;
-  color: #FF9F1C;
-  margin-bottom: 10vh;
+    font-size: 2rem;
+    margin-bottom: 10vh;
   }
+}
+  @media screen and (max-width: 300px) {
+    .cards{
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 4px;
+      row-gap: 20px;
+    }
+    .hero img {
+      height: 15vh;
+    }
+    .hero p {
+      font-size: 1rem;
+      margin-bottom: 10vh;}
+    }    
 </style>
 
 
 <div class="hero">
-    <img src="dante logo 2.png" alt="logo">
-    <p>Catalogo</p>
-    <div class="cards">
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-      <Card autore="Dante Alighieri" titolo="Divina Commedia"/>
-    </div>
+  <img src="dante logo 2.png" alt="logo">
+  <p>Catalogo</p>
+  <div class="cards">
+    <Card autore="Dante Alighieri" titolo="Divina Commedia" img="https://copertine.hoepli.it/archivio/978/8836/9788836005765.jpg"/>
+    <Card autore="Giovanni Boccaccio" titolo="Decameron" img="https://romis2016.files.wordpress.com/2020/03/boccaccio2.jpg"/>
+    <Card autore="Dan Brown" titolo="Il Codice da Vinci" img="https://www.mondadoristore.it/img/Il-Codice-da-Vinci-Dan-Brown/ea978880474667/BL/BL/01/ZOM/?tit=Il+Codice+da+Vinci&aut=Dan+Brown"/>
+    <Card autore="George Orwell" titolo="1984" img="https://assets.brightspot.abebooks.a2z.com/dims4/default/af999db/2147483647/strip/true/crop/328x493+0+4/resize/253x380!/quality/90/?url=http%3A%2F%2Fabebooks-brightspot.s3.us-west-2.amazonaws.com%2Ff6%2Fcc%2F4f7b32eb40454d5cfa3c2b13102c%2F1984.jpg"/>
+    <Card autore="James Joyce" titolo="Ulisse" img="https://www.lafeltrinelli.it/images/9788807903939_0_536_0_75.jpg"/>
+    <Card autore="Dante Alighieri" titolo="Divina Commedia" img="https://copertine.hoepli.it/archivio/978/8836/9788836005765.jpg"/>
+    <Card autore="Dante Alighieri" titolo="Divina Commedia" img="https://copertine.hoepli.it/archivio/978/8836/9788836005765.jpg"/>
+  </div>
 </div>
